c04129d589843d815ed146b51211fd0b0a3c05d2
GioTvaliashvili <babucidze86@gmail.com>
2025-02-25 21:32:50 +0400

    first commit

---
+++ day44/home/script.js
@@ -0,0 +1,35 @@
+
+
+
+alert(5 + 3);
+console.log(5 + 3);
+
+
+alert(10 - 4);
+console.log(10 - 4);
+
+
+alert(6 * 7);
+console.log(6 * 7);
+
+
+alert(20 / 5);
+console.log(20 / 5);
+
+
+alert(2 ** 3);
+console.log(2 ** 3);
+
+
+alert("Hello" + " World");
+console.log("Hello" + " World");
+
+
+for (let i = 10; i >= 0; i--) {
+    alert("The world will be destroyed in " + i + " seconds");
+    console.log("The world will be destroyed in " + i + " seconds");
+}
+
+
+
+ 
