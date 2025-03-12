fd206b777200c966304b875a4cc73cd83435f75c
GioTvaliashvili <tvaligio8@gmail.com>
2025-03-12 21:19:11 +0400

    commit

---
+++ day47/class/script.js
@@ -0,0 +1,19 @@
+//#1
+
+//let balance = 450000
+//let withdraw = 9000
+//console.log(balance - withdraw)
+
+//#2
+
+name = prompt("enter name:  ")
+
+lastname = prompt("enter lastname:  ")
+
+age = prompt("enter age:  ")
+
+address = prompt("enter address:  ")
+
+console.log("my name is" + " " + name + "and my lastname is " +  lastname + " " + "i am " + age + " " + "i live in" + address)
+
+
