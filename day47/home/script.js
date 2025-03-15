fddb9068a6240f26043d0217e967106a47071c60
GioTvaliashvili <babucidze86@gmail.com>
2025-03-15 18:58:58 +0400

    commit

---
+++ day47/home/script.js
@@ -0,0 +1,36 @@
+
+let name = prompt("enter name:  ")
+
+
+let lastname = prompt("enter lastname:  ")
+
+
+let age = prompt("enter age:  ")
+
+
+let address = prompt("enter address:  ")
+
+
+let momname = prompt("enter  momname:  ")
+
+
+let momlastname = prompt("enter momlastname:  ")
+
+
+let momage = prompt("enter momage:  ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log("my name is" + " " + name + "and my lastname is " +  lastname + " " + "i am " + age + " " + "i live in" + address + "." +' ' + "my mom's name is" + ' '+ momname + "," + ' ' + 'my moms lastname is' + ' ' + momlastname + ' ' + 'and my moms age is ' + ' ' + momage  )
