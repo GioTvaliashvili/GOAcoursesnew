435935c68cbbadcc46e608a7c486a59f3a25cb50
GioTvaliashvili <babucidze86@gmail.com>
2025-03-29 20:09:51 +0400

    commit

---
+++ day49/class/.js
@@ -0,0 +1,11 @@
+let age = 14
+
+
+if (age >= 19) {
+    console.log("Take your kindle");
+} else {
+    console.log("age is less than 19");
+}
+
+
+console.log({ red: 1, green: 2, black: 3 }["green"]);
---
+++ day49/home/script.js
@@ -0,0 +1,69 @@
+// //1.
+// let num1 = prompt("num1:");
+// let num2 = prompt("num2:");
+
+// console.log(num1 % 2 === 0 ? "ლუწი" : "კენტი");
+// console.log(num2 % 2 === 0 ? "ლუწი" : "კენტი");
+
+
+// 2.
+// let temp = Number(prompt("Enter temp:"));
+
+// if (temp <= 5) {
+
+//   console.log("It is too cold");
+
+// } else if (temp >= 45) {
+    
+
+//   console.log("It is too hot");
+
+// } else if (temp >= 30) {
+
+//   console.log("It is normal");
+
+// } else {
+
+//   console.log("Error"); 
+
+// }
+    
+
+3.
+function gradeStudent(score) {
+    if (score >= 90 && score <= 100) {
+
+      console.log("A");
+
+
+    } else if (score >= 80 && score <= 89) {
+
+      console.log("B");
+
+
+    } else if (score >= 70 && score <= 79) {
+
+      console.log("C");
+
+
+    } else if (score >= 60 && score <= 69) {
+
+      console.log("D");
+
+
+    } else if (score >= 0 && score <= 59) {
+
+
+      console.log("F");
+
+
+    } else {
+      console.log("Error");
+
+      
+
+
+    }
+  }
+  
+ 
