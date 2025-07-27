2fc73d13551d19e876b5b019e8cd721e3740dceb
GioTvaliashvili <babucidze86@gmail.com>
2025-07-27 14:02:22 +0400

    commit

---
+++ day65/home/.js
@@ -0,0 +1,32 @@
+//#1
+// let year = parseInt(prompt("Enter a year:"));
+
+// if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+//   console.log(`${year} is a leap year`);
+// } else {
+//   console.log(`${year} is not a leap year`);
+// }
+//#2
+// function whoPays(names) {
+//   const randomIndex = Math.floor(Math.random() * names.length);
+//   const payer = names[randomIndex];
+//   console.log(`${payer} will pay for everyone's food!`);
+// }
+
+
+// const nameList = ["გიო", "მარი", "ნინო", "ლაშა", "დათო"];
+// whoPays(nameList);
+
+//#3
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 3 === 0 && i % 5 === 0) {
+//     console.log("FizzBuzz");
+//   } else if (i % 3 === 0) {
+//     console.log("Fizz");
+//   } else if (i % 5 === 0) {
+//     console.log("Buzz");
+//   } else {
+//     console.log(i);
+//   }
+// }
+
