ca1800452ec127d707bab4b4e9caaf097ff4b3cb
GioTvaliashvili <babucidze86@gmail.com>
2025-10-18 15:26:11 +0400

    commit

---
+++ level78/home/js/main.js
@@ -0,0 +1,21 @@
+
+import { sum } from './app.js';
+
+
+console.log(sum(10, 20)); 
+
+
+
+import { multiply } from './app.js';
+
+console.log(multiply(4, 5)); 
+
+
+
+
+import { subtract, divide } from './app.js';
+
+
+console.log(subtract(10, 5));
+console.log(divide(10, 2));
+console.log(divide(10, 0));
