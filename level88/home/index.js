f5aa75e82137eb2f1bf7a32b349f800ca3be0f81
GioTvaliashvili <babucidze86@gmail.com>
2026-02-01 16:07:53 +0400

    commit

--- level88/home/index.js
+++ level88/home/index.js
@@ -1,7 +1,6 @@
-import { mkdir } from "node:fs";
-
-for (let i = 1; i < 6; i++) {
-  mkdir(`./folder_${i}`, (err) => {
+import {mkdir} from "node:fs"
+for (let i = 0; i < 6; i++) {
+  mkdir(`./lomi-${i}`, (err) => {
     console.log(err);
   });
 }
